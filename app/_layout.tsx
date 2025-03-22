76033edf2a8407463917651860a13116203adc50
Bismark Agyare <bismarkagyare54@gmail.com>
2025-03-22 19:02:18 +0000

    add navigation routes for Home and About screens in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,10 @@ import React from "react";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{title: "Home"}}/>
+      <Stack.Screen name="about" options={{title: "About"}}/>
+    </Stack>
+  )
 }

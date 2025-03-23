fb9e651ef45e875b68548ca4fa3f9f9d9e3a0c1d
Bismark Agyare <bismarkagyare54@gmail.com>
2025-03-23 09:23:46 +0000

    refactor Index component to enhance UI by replacing text link with image viewer and buttons for photo selection

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,37 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+
+
+const PlaceholderImage = require('../../assets/images/background-image.png');
+
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Home Screen</Text>
-      <Link style={styles.button} href="/about">Go to about page</Link>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme='primary'/>
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292E",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#25292e',
+    alignItems: 'center',
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
